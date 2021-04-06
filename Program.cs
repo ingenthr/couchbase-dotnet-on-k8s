e221495e89477e93571241024f9c1f270c48a8e8
Matt Ingenthron <ingenthr@cep.net>
2021-04-05 22:21:06 -0700

    Initial commit of working app in docker image.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using Couchbase;
+
+namespace repro
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            var cluster = await Cluster.ConnectAsync("couchbase://localhost", "username", "password");
+
+            var bucket = await cluster.BucketAsync("travel-sample");
+            var collection = bucket.DefaultCollection();
+
+
+            // var upsertResult = await collection.UpsertAsync("my-document-key", new { Name = "Ted", Age = 31 });
+            // var getResult = await collection.GetAsync("my-document-key");
+            // Console.WriteLine(getResult.ContentAs<dynamic>());
+
+            var queryResult = await cluster.QueryAsync<dynamic>("select \"Hello World\" as greeting");
+            await foreach (var row in queryResult) {
+                Console.WriteLine(row);
+            }
+        }
+    }
+}
